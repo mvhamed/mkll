ec352a7cd1d4b56fb27adf3d792eb62a489c52d6
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-21 01:31:25 +0300

    Update colladina.py

--- YukkiMusic/plugins/dina/colladina.py
+++ YukkiMusic/plugins/dina/colladina.py
@@ -5,12 +5,12 @@ from typing import Union
 from pyrogram.types import Message
 from YukkiMusic import app
 
-@app.on_message(filters.voice_chat_started)
+@app.on_message(filters.video_chat_started)
 async def babloo(client: Client, message: Message): 
       Startt = "- صعدوا نسمع أغاني 🫂"
       await message.reply_text(Startt)
 
-@app.on_message(filters.video_chat_started)
+@app.on_message(filters.voice_chat_ended)
 async def bablo(client: Client, message: Message): 
       Enddd = "- أصلاً مليت ☹"
       await message.reply_text(Enddd)
