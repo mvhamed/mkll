033c8b252e3fe84429f2392600e250863331a030
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-21 01:49:48 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -18,7 +18,7 @@ from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import YAFA_CHANNEL, YAFA_NAME
+
 from config import BANNED_USERS, lyrical
 from strings import get_command
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
@@ -36,29 +36,6 @@ from YukkiMusic.utils.inline.play import (livestream_markup,
 from YukkiMusic.utils.inline.playlist import botplaylist_markup
 from YukkiMusic.utils.logger import play_logs
 from YukkiMusic.utils.stream.stream import stream
-
-force_btn = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(   
-              text=f"{YAFA_NAME}", url=f"{YAFA_CHANNEL}",)
-        ],
-        [
-            InlineKeyboardButton(
-              text="• تحديثات السورس •", url="https://t.me/Mlze1bot",),                        
-        ],        
-    ]
-)
-async def check_is_joined(message):    
-    try:
-        userid = message.from_user.id
-        status = await app.get_chat_member(f"{CHANNEL_SUDO}", userid)
-        return True
-    except Exception:
-        await message.reply_text("**⚠️︙عذراً، عليك الانضمام الى القناة أولاً :**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
-        return False
-   
-
 
 
 # Command
