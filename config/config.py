5a11a43fbc441f7fc692ce560ded1f599fe4fe8d
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-21 01:04:54 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -80,7 +80,9 @@ SUPPORT_CHANNEL = getenv(
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", None
 )  # Example:- https://t.me/YukkiSupport
-
+ASAAQ_CHANNEL = getenv(
+   " ASAAQ_CHANNEL", "t.me/Mlze1bot"
+) 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
 
