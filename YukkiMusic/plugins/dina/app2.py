d043659a7eb86b31c651425f70723adff790da18
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-21 03:07:41 +0300

    Update app2.py

--- YukkiMusic/plugins/dina/app2.py
+++ YukkiMusic/plugins/dina/app2.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
  
-from YukkiMusic import app
+from YukkiMusic import app, userbot
 from YukkiMusic.core.call import Yukki
 
 
@@ -17,14 +17,14 @@ async def ass_leaveall(_, message: Message):
     left = 0
     failed = 0
     chats = []
-    async for dialog in Yukki.get_dialogs():
+    async for dialog in userbot.get_dialogs():
         chats.append(int(dialog.chat.id))
     schat = (await app.get_chat(chat_id))
     for i in chats:
         if i in (-1002037012482, int(schat)):
             continue
         try:
-            await Yukki.leave_chat(int(i))
+            await userbot.leave_chat(int(i))
             left += 1
         except FloodWait as e:
             flood_time = int(e.value)
