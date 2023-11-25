2db03e77f25e34127d41774115e5dad572578b05
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-25 04:59:57 +0300

    Update vdiena.py

--- YukkiMusic/plugins/dina/vdiena.py
+++ YukkiMusic/plugins/dina/vdiena.py
@@ -8,7 +8,6 @@ import asyncio
 from pyrogram import Client, filters
 from random import choice
 from strings import get_command
-from strings.filters import command
 from typing import Union
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import (InlineKeyboardButton,CallbackQuery,
