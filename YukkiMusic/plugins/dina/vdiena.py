da3a96466fd259daa531d03c6e5b90b7f27933c3
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-25 04:55:34 +0300

    Update vdiena.py

--- YukkiMusic/plugins/dina/vdiena.py
+++ YukkiMusic/plugins/dina/vdiena.py
@@ -4,7 +4,35 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from YukkiMusic import app, Telegram
 import random
 from config import ASAAQ_CHANNEL, YAFA_NAME
+import asyncio
+from pyrogram import Client, filters
+from random import choice
+from strings import get_command
+from strings.filters import command
+from typing import Union
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import (InlineKeyboardButton,CallbackQuery,
+                            InlineKeyboardMarkup, Message)
 
+
+
+RAEAK = ["زينه","حلوه","فخمه","جميله","خوش","انيقه","مو حلوه","بشعه","مو زينه","كلش حلوه","استمر بيها","احبها","غيرها حباب"]
+
+@app.on_message(filters.command(["صورتي","رائي دينا بصورتك"],"")) 
+async def madison(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""صورتك {choice(RAEAK)} 🧸♥️""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        ),
+)
 
 @app.on_message(filters.command([f"زومل", "زامل", "عيسى الليث", "{BOT_USERNAME} زامل"],"") & filters.private)
 async def ihd(client: Client, message: Message):
