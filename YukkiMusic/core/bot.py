6b42feee993259703f0c5f17e19f2f6d1df1ad2d
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-12-02 00:45:54 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -61,7 +61,7 @@ class YukkiBot(Client):
             except:
                 pass
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != ChatMemberStatus:
+        if a.status != "administrator":
             LOGGER(__name__).error(
                 "Please promote Bot as Admin in Logger Group"
             )
