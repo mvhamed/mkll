7ecf6961f3f40eec5c217a320f959da140a05209
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-12-01 23:15:56 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -26,7 +26,7 @@ from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils import seconds_to_min, time_to_seconds
 from YukkiMusic.utils.channelplay import get_channeplayCB
-from YukkiMusic.utils.database import is_video_allowed
+from YukkiMusic.utils.database import is_video_allowed, is_served_user
 from YukkiMusic.utils.decorators.language import languageCB
 from YukkiMusic.utils.decorators.play import PlayWrapper
 from YukkiMusic.utils.formatters import formats
