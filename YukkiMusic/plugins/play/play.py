82b24994309047577248dcccdc13cf62a1e48797
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-12-01 22:50:27 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -84,6 +84,9 @@ async def play_commnd(
     url,
     fplay,
 ):
+  
+    if not await check_is_joined(message):
+        return
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
